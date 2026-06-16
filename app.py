4820cf0be47e09090b9643a0cc626356597b7615
ANNAMDEVULA DURGA PRASAD <durgaprasadannamdevula41@gmail.com>
2026-06-16 15:42:01 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,37 @@
+import streamlit as st
+import joblib
+
+# Load model and vectorizer
+model = joblib.load("fake_news_model.pkl")
+vectorizer = joblib.load("vectorizer.pkl")
+
+st.title("📰 Fake News Detector for Students")
+
+news = st.text_area("Paste News Article Here")
+
+if st.button("Check News"):
+    if news:
+        transformed = vectorizer.transform([news])
+        prediction = model.predict(transformed)
+
+        if prediction[0] == 1:
+            st.success("✅ Real News")
+        else:
+            st.error("❌ Fake News")
+
+        words = news.split()
+        summary = " ".join(words[:50])
+
+        st.subheader("Summary")
+        st.write(summary)
+
+        st.subheader("Verification Tips")
+        st.write("""
+        ✓ Verify the source
+
+        ✓ Check publication date
+
+        ✓ Cross-check with trusted websites
+
+        ✓ Look for evidence and references
+        """)
